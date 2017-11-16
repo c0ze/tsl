266c77ea86fb6a0536cea143e1f62c69edf0b697
Arda Karaduman <akaraduman@gmail.com>
2017-11-16 23:56:27 +0900

    read nested yaml

--- yaml.c
+++ yaml.c
@@ -8,18 +8,26 @@ typedef struct Conf {
   char* db_user;
 };
 
-struct Conf* readConf(char*);
+typedef struct MassConf {
+  struct Conf* server1;
+  struct Conf* server2;
+};
+
+struct MassConf* readConf(char*);
 
 int main(int argc, char * argv[]){
-  struct Conf* cf = readConf("conf.yaml");
-  printf("cf: %s %s %s", cf->db_server, cf->db_pass, cf->db_user);
+  struct MassConf* cf = readConf("conf.yaml");
+  printf("cf1: %s %s %s\n", cf->server1->db_server, cf->server1->db_pass, cf->server1->db_user);
+  printf("cf2: %s %s %s\n", cf->server2->db_server, cf->server2->db_pass, cf->server2->db_user);
 }
   
-struct Conf* readConf(char* filename) {
+struct MassConf* readConf(char* filename) {
   FILE* fh = fopen(filename, "r");
   yaml_parser_t parser;
   yaml_token_t token;
-  struct Conf* conf = malloc(sizeof(struct Conf));
+  struct MassConf* conf = malloc(sizeof(struct MassConf));
+  conf->server1 = malloc(sizeof(struct Conf));
+  conf->server2 = malloc(sizeof(struct Conf));
 
   if (!yaml_parser_initialize(&parser))
     fputs("Failed to initialize parser!\n", stderr);
@@ -35,6 +43,7 @@ struct Conf* readConf(char* filename) {
      */
 
     char** datap;
+    struct Conf* confp;
     char* tk;
 
     yaml_parser_scan(&parser, &token);
@@ -56,11 +65,15 @@ struct Conf* readConf(char* filename) {
         /* It's safe to not use strncmp as one string is a literal */
         if (!strcmp(tk, "db_server")) {
 	  //	  printf("got db_server");
-          datap = &conf->db_server;
+          datap = &confp->db_server;
         } else if (!strcmp(tk, "db_pass")) {
-          datap = &conf->db_pass;
+          datap = &confp->db_pass;
         } else if (!strcmp(tk, "db_user")) {
-          datap = &conf->db_user;
+          datap = &confp->db_user;
+        } else if (!strcmp(tk, "server1")) {
+          confp = conf->server1;
+        } else if (!strcmp(tk, "server2")) {
+          confp = conf->server2;
         } else {
           printf("Unrecognised key: %s\n", tk);
         }
